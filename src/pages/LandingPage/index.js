c9710d370b29afc4d65b1fb7af836f8c8dbfb5db
Muhammad Haekal <muhammad.haekal@shopee.com>
2019-12-07 18:25:03 +0700

    Centering Web Content and Add Max Width

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import NotifPanel from "../../components/NotifPanel";
-import { Wrapper } from "./styled";
+import { Wrapper, CenterDiv } from "./styled";
+import Jumbotron from "../../components/Jumbotron";
 
 const LandingPage = () => {
   return (
     <Wrapper>
       <NotifPanel></NotifPanel>
+      <CenterDiv>
+        <Jumbotron></Jumbotron>
+      </CenterDiv>
     </Wrapper>
   );
 };
--- src/pages/LandingPage/styled.js
+++ src/pages/LandingPage/styled.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  height: 2000px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const CenterDiv = styled.div`
+  padding-top: 66px;
+  max-width: 1366px;
+  width: 100%;
 `;
