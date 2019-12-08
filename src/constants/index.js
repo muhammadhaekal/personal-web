e2718f259d2ad65777c5a50ba0d9c2c8beb605f4
Muhammad Haekal <muhammad.haekal@shopee.com>
2019-12-08 12:32:41 +0700

    Add skills as Constant Variable

---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+export const skills = [
+  {
+    name: "consult",
+    description:
+      "Our work then targeted, best practices outcomes social innovation synergy. Venture philanthropy, revolutionary inclusive policymaker relief. User-centered program areas scale.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Design",
+    description:
+      "Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Develop",
+    description:
+      "Revolutionary circular, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Marketing",
+    description:
+      "Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Manage",
+    description:
+      "Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Evolve",
+    description:
+      "Activate catalyze and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes big data, fairness, social game-changer.",
+    class: "fas fa-comments"
+  }
+];
